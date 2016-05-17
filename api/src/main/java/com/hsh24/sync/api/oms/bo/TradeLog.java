b9898ba65f11d64595caa67292c63c2a026a3a26
Jiakun Xu <jiakunxu@gmail.com>
2016-05-17 15:15:40 +0800

    接口

--- api/src/main/java/com/hsh24/sync/api/oms/bo/TradeLog.java
+++ api/src/main/java/com/hsh24/sync/api/oms/bo/TradeLog.java
@@ -1,5 +1,50 @@
 package com.hsh24.sync.api.oms.bo;
 
+/**
+ * 
+ * @author JiakunXu
+ * 
+ */
 public class TradeLog {
+
+	private Long Id;
+
+	private Long tradeId;
+
+	private String type;
+
+	private String modifyUser;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public Long getTradeId() {
+		return tradeId;
+	}
+
+	public void setTradeId(Long tradeId) {
+		this.tradeId = tradeId;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getModifyUser() {
+		return modifyUser;
+	}
+
+	public void setModifyUser(String modifyUser) {
+		this.modifyUser = modifyUser;
+	}
 
 }
