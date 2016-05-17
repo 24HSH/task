7591666198dec52d213ab55d08d240e0edfef08b
alan-zhou <alan-zhou@192.168.1.22>
2016-05-17 14:23:51 +0800

    task

---
+++ task/src/main/java/com/hsh24/sync/oms/task/OrderStateSyncTask.java
@@ -0,0 +1,13 @@
+package com.hsh24.sync.oms.task;
+
+import com.hsh24.sync.framework.log.Logger4jCollection;
+import com.hsh24.sync.framework.log.Logger4jExtend;
+
+public class OrderStateSyncTask {
+	private Logger4jExtend logger = Logger4jCollection.getLogger(OrderStateSyncTask.class);
+
+	
+	public void  orderStateSync(){
+		
+	}
+}
